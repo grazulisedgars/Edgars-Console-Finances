780a38f274459c03291f9702acebfdf9413ed4d8
grazulisedgars <grazulisedgars@gmail.com>
2023-10-20 23:17:58 +0300

    Calculates total months

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+console.log("This ting works!");
+
+
 var finances = [
   ['Jan-2010', 867884],
   ['Feb-2010', 984655],
@@ -86,3 +89,23 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+
+
+// The total number of months included in the dataset.
+
+console.log("Total months: " + finances.length);
+
+
+
+// The net total amount of Profit/Losses over the entire period.
+
+// The average of the changes in Profit/Losses over the entire period.
+
+// You will need to track what the total change in profits is from month to month and then find the average.
+
+// (Total/(Number of months - 1))
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+// The greatest decrease in losses (date and amount) over the entire period.
